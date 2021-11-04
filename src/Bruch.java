832d6625df2ee0529c12a54fdaa9f1d6846af828
valentinschintler <s51125@edu.campus02.at>
2021-11-04 13:54:27 +0100

    toDecimal

--- src/Bruch.java
+++ src/Bruch.java
@@ -20,7 +20,7 @@ public class Bruch {
     public double toDecimal() {
         // TODO: Die Methode soll den Dezimalwert zum Bruch retournieren
         // Achtung: Ganzzahldivision!
-        return 0;
+        return (double) numerator / denominator;
     }
 
     public String print() {
